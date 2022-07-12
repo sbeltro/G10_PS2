b915e081f6642c060a21d0925978519f4fda7074
sbeltro <sbeltro@gmail.com>
2022-07-12 17:23:08 -0500

    Encabezado del código

---
+++ scripts/problem_set_02.R
@@ -0,0 +1,5 @@
+################################################################################
+# Problem Set 2: Predicting Poverty
+# Autores: Natalia Capacho, Yurani Gonzalez, Sebastian Beltran
+# Big Data - MECA 
+################################################################################
